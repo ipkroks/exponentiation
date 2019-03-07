e7d042ed341f4ba2ef81cfde131999831a383854
Igor <ipkroks@gmail.com>
2019-03-07 16:23:34 +0300

    bugfix

--- Exponentiation.php
+++ Exponentiation.php
@@ -4,14 +4,14 @@ namespace ipkroks\exponentiation;
 
 class Exponentiation {
 	public function go($number) {
-		return $number * 2;
+		return $number ** 2;
 	}
 
 	public function go3($number) {
-		return $number * 3;
+		return $number ** 3;
 	}
 
 	public function go4($number) {
-		return $number * 4;
+		return $number ** 4;
 	}
 }
