9b09ce7b8d088c04ea738a3f321d4ea8b1892af2
Igor Popov <ipkroks@gmail.com>
2021-04-01 17:22:32 +0300

    upd

--- tests/ExponentationTest.php
+++ tests/ExponentationTest.php
@@ -10,6 +10,6 @@ class ExponentiationTest extends TestCase
 {
     public function testGo(){
         $exponant = new Exponentiation();
-        $this->assertEquals($exponant->go(3), 9);
+        $this->assertEquals($exponant->go(4), 9);
     }
 }
